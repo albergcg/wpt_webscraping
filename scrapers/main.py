5c8e8a895110a43ab8a0d9c84c7ac29b49bd11d2
Alberto Garcia Galindo <33427700+albergcg@users.noreply.github.com>
2020-11-08 16:55:51 +0100

    Update main.py

--- scrapers/main.py
+++ scrapers/main.py
@@ -4,4 +4,4 @@ import scraper
 
 chrome_driver_path = 'D:\Alberto\Archivos de programa\chromedriver.exe'
 wpt_scraper = scraper.WPTScraper(topX=100)
-wpt_scraper.start_scraper()
+wpt_scraper.start_scraper(driver_path=chrome_driver_path)
