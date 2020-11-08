fda016893d776a28bd4c063030e5d899abab6091
Alberto Garcia Galindo <33427700+albergcg@users.noreply.github.com>
2020-11-08 16:48:39 +0100

    Update main.py

--- scrapers/main.py
+++ scrapers/main.py
@@ -2,5 +2,6 @@ import sys
 sys.path.append(0, '../')
 import scraper
 
+chrome_driver_path = 'D:\Alberto\Archivos de programa\chromedriver.exe'
 wpt_scraper = scraper.WPTScraper(topX=100)
 wpt_scraper.start_scraper()
